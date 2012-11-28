8011a34b4b15b3fc6cd2f1676fa6e755d655ff4c
Adam Coddington <me@adamcoddington.net>
2012-11-27 19:19:39 -0800

    Bumping version number.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-location',
-    version='1.0.3',
+    version='1.1',
     url='http://bitbucket.org/latestrevision/django-location/',
     description='Gather, store, and display real-time location information from Foursquare, Google Latitude, and more.',
     author='Adam Coddington',
