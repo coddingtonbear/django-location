0cd39a355cc7b4ae4a3bfbb55ff3bba1ff609826
Adam Coddington <me@adamcoddington.net>
2013-05-18 01:46:03 -0700

    Adding functionality for gathering device location from Apple's iCloud service.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-location',
-    version='1.3.2',
+    version='1.4',
     url='http://bitbucket.org/latestrevision/django-location/',
     description='Gather, store, and display real-time location information from Foursquare, Google Latitude, and more.',
     author='Adam Coddington',
@@ -26,14 +26,20 @@ setup(
             ],
             'kml': [
                 'pykml',
+            ],
+            'icloud': [
+                'pyicloud', # Currently, pending action from the package
+                            # maintainer, this is not distributed on PyPI,
+                            # you can, though, manually install this package
+                            # from https://github.com/latestrevision/pyicloud
             ]
         },
     install_requires=[
         'django>=1.4',
         'django-social-auth',
         'pytz',
-        'requests',
-        'jsonfield',
+        'requests>=1.2.0',
+        'jsonfield>=0.9.15',
         'lxml',
         ],
     packages=[
