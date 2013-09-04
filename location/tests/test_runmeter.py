509b0da030687798f148203982d1fda410fd3553
Adam Coddington <me@adamcoddington.net>
2013-09-03 19:54:52 -0700

    Adding runmeter tests.
    --HG--
    branch : two_point_oh

--- location/tests/test_runmeter.py
+++ location/tests/test_runmeter.py
@@ -6,7 +6,7 @@ from django.contrib.gis.geos import Point
 from django.utils.timezone import utc
 from django_mailbox.models import Mailbox, Message
 from lxml import objectify
-from mock import MagicMock
+from mock import MagicMock, patch
 
 from location import models
 from location.tests.base import BaseTestCase
@@ -35,7 +35,14 @@ class RunmeterTest(BaseTestCase):
         with open(file_path, 'r') as incoming:
             return objectify.fromstring(incoming.read())
 
-    def test_process_message(self):
+    @patch.object(RunmeterConsumer, 'get_import_url_from_message_body')
+    @patch.object(RunmeterConsumer, 'get_source_from_user_and_url')
+    @patch.object(RunmeterConsumer, 'message_indicates_finish')
+    @patch.object(RunmeterConsumer, 'process')
+    def test_process_message(
+        self, process, message_indicates_finish,
+        get_source_from_user_and_url, get_import_url_from_message_body
+    ):
         arbitrary_body = 'I am the very model of a modern major general'
         arbitrary_url = 'http://go.com/'
         arbitrary_source = models.LocationSource.objects.create(
@@ -49,16 +56,15 @@ class RunmeterTest(BaseTestCase):
             from_header=self.arbitrary_email,
             body=arbitrary_body,
         )
-        RunmeterConsumer.get_import_url_from_message_body = MagicMock(
-            return_value=arbitrary_url
+        get_import_url_from_message_body.return_value = (
+            arbitrary_url
         )
-        RunmeterConsumer.get_source_from_user_and_url = MagicMock(
-            return_value=arbitrary_source
+        get_source_from_user_and_url.return_value = (
+            arbitrary_source
         )
-        RunmeterConsumer.message_indicates_finish = MagicMock(
-            return_value=False
+        message_indicates_finish.return_value = (
+            False
         )
-        RunmeterConsumer.process = MagicMock()
 
         RunmeterConsumer.process_message(ok_message)
 
@@ -113,8 +119,137 @@ class RunmeterTest(BaseTestCase):
             expected_value,
         )
 
-    def test_process(self):
-        pass
+    def test_process_new_source(self):
+        arbitrary_url = 'http://www.go.com/101'
+        arbitrary_route_name = 'Something'
+        arbitrary_source = models.LocationSource.objects.create(
+            name='Whatnot',
+            user=self.user,
+            type=self.source_type,
+            active=True,
+            data={
+                'url': arbitrary_url,
+                'known_points': {},
+            }
+        )
+        arbitrary_document = MagicMock()
+        arbitrary_time = datetime.datetime.utcnow().replace(
+            tzinfo=utc
+        )
+        arbitrary_points = [
+            {'lat': -122, 'lng': 45, 'key': 'alpha', 'time': 1},
+            {'lat': -123, 'lng': 44, 'key': 'beta', 'time': 2}
+        ]
+
+        consumer = RunmeterConsumer(arbitrary_source)
+        consumer._get_document = MagicMock(
+            return_value=arbitrary_document
+        )
+        consumer.get_start_time = MagicMock(
+            return_value=arbitrary_time
+        )
+        consumer.get_route_name = MagicMock(
+            return_value=arbitrary_route_name
+        )
+        consumer.get_points = MagicMock(
+            return_value=arbitrary_points
+        )
+        consumer.is_active = MagicMock(
+            return_value=False
+        )
+
+        consumer.process()
+
+        consumer._get_document.assert_called_with(arbitrary_url)
+        consumer.get_start_time.assert_called_with(arbitrary_document)
+        consumer.get_route_name.assert_called_with(arbitrary_document)
+        consumer.get_points.assert_called_with(arbitrary_document)
+
+        actual_points = models.LocationSnapshot.objects.order_by('date')
+        self.assertEqual(actual_points.count(), 2)
+
+        first_assertions = {
+            'date': arbitrary_time + datetime.timedelta(seconds=1),
+            'source': arbitrary_source,
+            'location': Point(-122, 45)
+        }
+        for k, v in first_assertions.items():
+            self.assertEqual(getattr(actual_points[0], k), v)
+
+        second_assertions = {
+            'date': arbitrary_time + datetime.timedelta(seconds=2),
+            'source': arbitrary_source,
+            'location': Point(-123, 44)
+        }
+        for k, v in second_assertions.items():
+            self.assertEqual(getattr(actual_points[1], k), v)
+
+        self.assertFalse(
+            models.LocationSource.objects.get(pk=arbitrary_source.pk).active
+        )
+
+    def test_process_existing_source(self):
+        arbitrary_url = 'http://www.go.com/101'
+        arbitrary_route_name = 'Something'
+        arbitrary_source = models.LocationSource.objects.create(
+            name='Whatnot',
+            user=self.user,
+            type=self.source_type,
+            active=True,
+            data={
+                'url': arbitrary_url,
+                'known_points': {
+                    'alpha': 'arbitrary_value'
+                },
+            }
+        )
+        arbitrary_document = MagicMock()
+        arbitrary_time = datetime.datetime.utcnow().replace(
+            tzinfo=utc
+        )
+        arbitrary_points = [
+            {'lat': -122, 'lng': 45, 'key': 'alpha', 'time': 1},
+            {'lat': -123, 'lng': 44, 'key': 'beta', 'time': 2}
+        ]
+
+        consumer = RunmeterConsumer(arbitrary_source)
+        consumer._get_document = MagicMock(
+            return_value=arbitrary_document
+        )
+        consumer.get_start_time = MagicMock(
+            return_value=arbitrary_time
+        )
+        consumer.get_route_name = MagicMock(
+            return_value=arbitrary_route_name
+        )
+        consumer.get_points = MagicMock(
+            return_value=arbitrary_points
+        )
+        consumer.is_active = MagicMock(
+            return_value=True
+        )
+
+        consumer.process()
+
+        consumer._get_document.assert_called_with(arbitrary_url)
+        consumer.get_start_time.assert_called_with(arbitrary_document)
+        consumer.get_route_name.assert_called_with(arbitrary_document)
+        consumer.get_points.assert_called_with(arbitrary_document)
+
+        actual_points = models.LocationSnapshot.objects.order_by('date')
+        self.assertEqual(actual_points.count(), 1)
+
+        assertions = {
+            'date': arbitrary_time + datetime.timedelta(seconds=2),
+            'source': arbitrary_source,
+            'location': Point(-123, 44)
+        }
+        for k, v in assertions.items():
+            self.assertEqual(getattr(actual_points[0], k), v)
+
+        self.assertTrue(
+            models.LocationSource.objects.get(pk=arbitrary_source.pk).active
+        )
 
     def test_get_source_from_user_and_url_new(self):
         arbitrary_url = 'http://www.go.com/100'
